b127735708e5bbdf7b82cb982c1b62645c8b9e13
polojerry <polojerry4@gmail.com>
2019-11-22 09:05:18 +0300

    Friends List Design Added

--- app/src/main/java/com/polotechnologies/myfriends/fragments/FriendsListFragment.kt
+++ app/src/main/java/com/polotechnologies/myfriends/fragments/FriendsListFragment.kt
@@ -1,4 +1,4 @@
-package com.polotechnologies.myfriends
+package com.polotechnologies.myfriends.fragments
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.polotechnologies.myfriends.R
 
 /**
  * A simple [Fragment] subclass.
