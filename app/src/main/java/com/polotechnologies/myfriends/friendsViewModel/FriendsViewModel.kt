6cdee12565361a7a3407969f783d4f9026178a03
polojerry <polojerry4@gmail.com>
2019-11-25 10:55:05 +0300

    Added Function in FriendsViewHolder for Inserting, Updating and Fetching friends

--- app/src/main/java/com/polotechnologies/myfriends/friendsViewModel/FriendsViewModel.kt
+++ app/src/main/java/com/polotechnologies/myfriends/friendsViewModel/FriendsViewModel.kt
@@ -2,8 +2,49 @@ package com.polotechnologies.myfriends.friendsViewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.polotechnologies.myfriends.database.Friend
 import com.polotechnologies.myfriends.database.FriendsDatabaseDAO
+import kotlinx.coroutines.*
 
 class FriendsViewModel(
     val database: FriendsDatabaseDAO,
-    application: Application): AndroidViewModel(application)
+    application: Application): AndroidViewModel(application){
+
+    private var viewModelJob = Job()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+    val friends = database.getAllFriends()
+
+    fun startFriendship(){
+        uiScope.launch {
+            val newFriend = Friend()
+            insertFriend(newFriend)
+        }
+    }
+
+    private suspend fun insertFriend(friend : Friend){
+        withContext(Dispatchers.IO){
+            database.insertFriend(friend)
+        }
+
+    }
+
+    fun updateFriendship(){
+        uiScope.launch {
+            val newFriend = Friend()
+            insertFriend(newFriend)
+        }
+    }
+
+    private suspend fun updateFriend(friend : Friend){
+        withContext(Dispatchers.IO){
+            database.updateFriend(friend)
+        }
+
+    }
+}
